63e2d22fdf66fb4ef9b8358351a8c64075182e63
Michael Marvick <mmarvick@gmail.com>
2015-02-27 18:45:50 -0800

    Add location detection

--- MainController.js
+++ MainController.js
@@ -6,6 +6,20 @@
 
   var MainController = function($scope, $location) {
 
+    var getLocation = function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setCurrentPosition);
+      }
+    };
+
+    var setCurrentPosition = function(position) {
+      $scope.$apply(function() {
+        $scope.haveLocation = true;
+        $scope.currentLatitude = position.coords.latitude;
+        $scope.currentLongitude = position.coords.longitude;
+      });
+    };
+
     $scope.search = function(distance, latitude, longitude) {
       url = "/search"
 
@@ -26,8 +40,17 @@
       }
       $location.path(url);
     }
+
+    $scope.useCurrentLocation = function() {
+      $scope.latitude = $scope.currentLatitude;
+      $scope.longitude = $scope.currentLongitude;
+    }
+
+    getLocation();
+
+
   };
-  
+
   app.controller("MainController", ["$scope", "$location", MainController]);
 
 }());
